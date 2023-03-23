96b076c4169e7824e8c89c75202f85984cd256ea
Anjula Perera <anjulaperera98@gmail.com>
2023-03-23 12:15:20 +0530

    home page to front page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('front');
 });
 Route::get('/tasks','App\Http\Controllers\TasksController@index' );
 
